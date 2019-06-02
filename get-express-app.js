2904c65b53433d4a19a20eb67f665a6abcbcb4ba
Carles Capellas Careta <capellas.carles@gmail.com>
2019-06-02 16:14:03 +0200

    Make the project runnable as standalone

--- get-express-app.js
+++ get-express-app.js
@@ -5,6 +5,10 @@ const matchesController = require('./controllers/matches-controller');
 module.exports = () => {
 	const app = express();
 	app.use('/', express.static(join(__dirname, 'public')));
+	app.use(
+		'/dragon-pong',
+		(req, res, next) => res.sendFile(join(__dirname, 'public', 'index.html'))
+	);
     app.get('/matches', matchesController.getAll);
 	return app;
 };
